e72989c9e48d791e4a7f942fc8efb19e631a8a0e
GabrielIGNS <gabrielgranjeia@alunos.utfpr.edu.br>
2023-09-13 15:18:55 -0300

    exemplo do professor

--- aula-ts-10/src/index.ts
+++ aula-ts-10/src/index.ts
@@ -14,3 +14,27 @@ console.log(identity<string>("Hello"));
 console.log(identity<number[]>([1,2,3]));
 console.log(identity<undefined>(null));
 console.log(identity<null>(undefined));
+
+//exemplo do professor:
+type Collection<TIPO> = {
+    content: TIPO[]
+}
+
+type Card = {
+    title: string;
+}
+
+const collection: Collection<Card> = {
+    content: [{
+        title: "card1"
+    }, {
+        title: "card2"
+}]
+}
+
+const collection2: Collection<string> = {
+    content: ["card1", "card2"]
+}
+
+console.log(collection)
+console.log(collection2)
